951b2990ca64703c89454456ff7cbfe06dc4977a
huylem1800vn <huylem1800vn@gmail.com>
2024-03-25 12:18:08 +0700

    Cấu hình cho folder routes

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
+
+const route = require("./routes/index.route");
+
 const app = express();
 const port = 3000;
 
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-app.get('/', (req, res) => {
-    res.render("client/pages/home/index")
-  });
-
-app.get('/products', (req, res) => {
-  res.render("client/pages/products/index")
-  });
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
