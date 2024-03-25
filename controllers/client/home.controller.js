c058b683c6b5b9b178dcee4a22220e1a315f2aa6
huylem1800vn <huylem1800vn@gmail.com>
2024-03-25 14:19:34 +0700

    cấu trúc lại folder pug

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,9 @@
 // [GET] /
 
 module.exports.index = (req, res) => {
-    res.render("client/pages/home/index", {
+    res.render("client/pages/home/index", 
+    {
       pageTitle : "Trang chủ"
-    })
+    }
+    )
   };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,9 @@
 // [GET] /products/
 
 module.exports.index = (req, res) => {
-    res.render("client/pages/products/index", {
+    res.render("client/pages/products/index", 
+    {
       pageTitle : "Danh sách sản phẩm"
-    })
+    }
+    )
   };
