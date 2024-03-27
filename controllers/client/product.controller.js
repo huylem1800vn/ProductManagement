7899de6095592edafca6b79477f588e344146eec
huylem1800vn <huylem1800vn@gmail.com>
2024-03-27 13:40:45 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,9 +1,25 @@
-// [GET] /products/
+const Product = require("../../models/product.model");
 
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index", 
-    {
-      pageTitle : "Danh sách sản phẩm"
-    }
+// [GET] /products/
+// models là folder chứa các file dùng để truy vấn các database
+// controller dùng để chọc vào model để lấy data 
+// database trong đó
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  });
+
+  for (const item of products) {
+    item.priceNew = 
+    (item.price * (100 - item.discountPercentage) / 100).toFixed(0); 
+  };
+
+  res.render("client/pages/products/index", 
+  {
+    pageTitle : "Danh sách sản phẩm",
+    products: products
+  }
     )
   };
