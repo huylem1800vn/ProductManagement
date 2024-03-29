39eb5b722c6f1302441f375f0354c172246923af
huylem1800vn <huylem1800vn@gmail.com>
2024-03-29 14:45:48 +0700

    Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,9 +1,18 @@
+const Product = require("../../models/product.model");
+
 // [GET] /admin/products/
 
-module.exports.index = (req, res) => {
-    res.render("admin/pages/products/index", 
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });// Trả ra tất cả các bản ghi trong database
+
+  console.log(products);
+
+  res.render("admin/pages/products/index", 
     {
-      pageTitle : "Tramg danh sách sản phẩm"
+      pageTitle : "Tramg danh sách sản phẩm",
+      products: products
     }
     )
   };
