65e3b88a6f14e1a469394dd9aa2af21385e35d9c
huylem1800vn <huylem1800vn@gmail.com>
2024-04-05 09:39:55 +0700

    Hiển thị thông báo sau khi đổi trạng thái(Xử lý ẩn thông báo bên Frontend)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -189,3 +189,21 @@ if(listButtonDelete.length > 0) {
     });
 }
 // end button-delete 
+
+// show-alert
+const showAlert = document.querySelector("[show-alert]");
+if(showAlert) { 
+    const time = parseInt(showAlert.getAttribute("data-time"));
+    
+    // Sau time giây sẽ đóng thông báo
+    setTimeout( () => {
+        showAlert.classList.add("alert-hidden");
+    }, time);
+
+    // Khi click vào nút sẽ đóng luôn
+    const closeAlert = showAlert.querySelector("[close-alert]");
+    closeAlert.addEventListener("click", () => {
+        showAlert.classList.add("alert-hidden");
+    }) 
+}
+// end show-alert
