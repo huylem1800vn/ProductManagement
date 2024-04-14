6c064b6e3622dcfc9a024cf2a0a452e2c914ca5c
huylem1800vn <huylem1800vn@gmail.com>
2024-04-14 23:02:44 +0700

    Xây dựng phần quản lý danh mục sản phẩm (Phân cấp danh mục trang thêm mới danh mục)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,5 +1,6 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system")
+const createTreeHelper = require("../../helpers/createTree.helper");
 
 // [GET] /admin/products-category/
 module.exports.index = async (req, res) => {
@@ -19,11 +20,14 @@ module.exports.create = async (req, res) => {
     const records = await ProductCategory.find({
         deleted: false,
     });
-    console.log(records);
+
+    const newRecords = createTreeHelper(records);// tạo ra cây lồng nhau
+
+    // console.log(newRecords);
 
     res.render("admin/pages/products-category/create", {
         pageTitle: "Thêm mới danh mục sản phẩm",
-        records: records,
+        records: newRecords,
     });
 };
 
