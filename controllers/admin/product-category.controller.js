a72af4af376e9f3afab9e03e3a47740086f0a35e
huylem1800vn <huylem1800vn@gmail.com>
2024-04-15 11:30:12 +0700

    Tính năng chỉnh sửa danh mục sản phẩm (Lưu data vào database)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -79,3 +79,20 @@ module.exports.edit = async (req, res) => {
         res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
     }
 }
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    req.body.position = parseInt(req.body.position);
+
+    try {
+        await ProductCategory.updateOne({_id: id,}, req.body)
+        req.flash("success", `Cập nhật danh mục ${req.body.title} thành công!`)
+    } catch (error) {
+        // khi người dùng sửa id trên url khiến cho id bị sai thì sẽ bắt vào catch để không bị lỗi hệ thống
+        req.flash("error", `Cập nhật danh mục ${req.body.title} không thành công!`)
+    }
+
+    res.redirect("back");
+}
