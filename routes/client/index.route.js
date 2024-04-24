033cfea48c3cda55da14c6c87363dd76305f2b48
huylem1800vn <huylem1800vn@gmail.com>
2024-04-24 12:59:33 +0700

    Hiển thị menu chung bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
 module.exports = (app) => {
+    // để tất cả các trang đều chạy qua middleware này
+    app.use(categoryMiddleware.category)
+
     app.use('/', homeRoutes);
     
     app.use('/products', productRoutes);
