cf9ab5d3c3ee78f43fef03630af40deb541e64dc
huylem1800vn <huylem1800vn@gmail.com>
2024-04-25 14:33:55 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -12,10 +12,19 @@ module.exports.index = async (req, res) => {
     product.priceNew = (product.price * (100 - product.discountPercentage)/100).toFixed(0);
   }
 
-  console.log(productsFeatured)
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc" }).limit(6).select("-description");// select là lấy ra các trường và cách nhau khoảng trắng,  muốn bỏ trường nào thì cho dấu - trước nó
+
+  for (const product of productsNew) {
+    product.priceNew = (product.price * (100 - product.discountPercentage)/100).toFixed(0);
+  }
+
   
   res.render("client/pages/home/index", {
     pageTitle : "Trang chủ",
     productsFeatured: productsFeatured,
+    productsNew: productsNew,
   });
 };
