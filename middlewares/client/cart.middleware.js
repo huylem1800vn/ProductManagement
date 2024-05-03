5464eaaa8e08dd0412e6af9cb68f02905e767d0f
huylem1800vn <huylem1800vn@gmail.com>
2024-05-03 14:59:46 +0700

    Làm nút minicart(Giỏ hàng) trên header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -9,6 +9,13 @@ module.exports.cart = async (req, res, next) => {
 
         // Trả về cookie một biến cartId: cart.id
         res.cookie("cartId", cart.id);
+    } else {
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId,
+        });
+        console.log(cart);
+        // trả ra một biến miniCart ra locals để tất cả các trang có chạy qua middleware này đều lấy được
+        res.locals.miniCart = cart;
     }
     
 
